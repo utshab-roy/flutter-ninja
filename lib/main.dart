90a036701c4992dc457ec26b3ee96d1e5d01b142
Utshab Roy <utshab.roy@gmail.com>
2021-03-09 17:32:22 +0900

    shortcut of assets adding

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,8 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Image(
-          // image: NetworkImage(
-          //     'https://images.unsplash.com/photo-1615194787278-25ca9b7c5a16?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80'),
-          image: AssetImage('assets/sample-2.jpg'),
-        ),
+        child: Image.asset('assets/sample-2.jpg'),
+        // child: Image.network('image-link'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
