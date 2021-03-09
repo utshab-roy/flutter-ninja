cfe2525994a40583ee649eb0d67a0eba2d6c7373
Utshab Roy <utshab.roy@gmail.com>
2021-03-09 16:34:05 +0900

    new font added

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ void main() {
             fontWeight: FontWeight.bold,
             letterSpacing: 2.0,
             color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
           ),
         ),
       ),
