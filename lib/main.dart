9a5bcf641de5be185738aedf59c7b5631a57dfc9
Utshab Roy <utshab.roy@gmail.com>
2021-03-10 10:08:55 +0900

    IconButton implemented

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,12 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: RaisedButton.icon(
-          onPressed: () {},
-          icon: Icon(Icons.mail),
-          label: Text('Mail me'),
-          color: Colors.amber,
+        child: IconButton(
+          onPressed: () {
+            print('You Clicked me !');
+          },
+          icon: Icon(Icons.alternate_email),
+          color: Colors.blueAccent,
         ),
       ),
       floatingActionButton: FloatingActionButton(
