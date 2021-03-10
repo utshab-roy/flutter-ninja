8d0893f4a9c88a48a7283acf662d9149ed9cdf56
Utshab Roy <utshab.roy@gmail.com>
2021-03-10 16:45:06 +0900

    delete function implemented in quotes and passing Func as parameter

--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -3,8 +3,9 @@ import 'quote.dart';
 
 class QuoteCard extends StatelessWidget {
   final Quote quote;
+  final Function delete;
 
-  QuoteCard({this.quote});
+  QuoteCard({this.quote, this.delete});
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +31,15 @@ class QuoteCard extends StatelessWidget {
                 fontSize: 14.0,
                 color: Colors.grey[800],
               ),
+            ),
+            SizedBox(height: 8.0),
+            FlatButton.icon(
+              //we can not delete the data from here because it is not
+              //in this class, we make delete function, and using that function
+              //we are deleting the data, we are passing function as parameter
+              onPressed: delete,
+              label: Text('Delete Quote'),
+              icon: Icon(Icons.delete),
             ),
           ],
         ),
