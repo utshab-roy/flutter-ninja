0f8c28598c10cce17faf9c1dd6391898ee94bb13
Utshab Roy <utshab.roy@gmail.com>
2021-03-17 18:24:54 +0900

    clear the branch for new project

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,16 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: QuoteList(),
+    home: Home(),
   ));
 }
 
-class QuoteList extends StatefulWidget {
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote(author: 'oscar', text: 'Orcar give a quot2e'),
-    Quote(author: 'adam', text: 'this is Adams quote'),
-  ];
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Awesome Quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        children: quotes
-            .map((quote) => QuoteCard(
-                  quote: quote,
-                  delete: () {
-                    setState(() {
-                      quotes.remove(quote);
-                    });
-                  },
-                ))
-            .toList(),
-      ),
+    return Container(
+      child: Text('Home Page !'),
     );
   }
 }
