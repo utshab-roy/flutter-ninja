f5434fe4cf8da2a970bcf5bb804a77da005cf8a6
Utshab Roy <utshab.roy@gmail.com>
2021-03-17 10:45:29 +0900

    firebase setup, connect with app and basic structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ninja/screens/wrapper.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    home: Wrapper(),
   ));
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.red[200],
-      appBar: AppBar(
-        title: Text('Firebase App'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-    );
-  }
 }
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ninja/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return either home or authenticate widget
+    return Home();
+  }
+}
