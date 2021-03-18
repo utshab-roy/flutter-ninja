acab9a89f11e08d0b1630abb35c60bae5458215a
Utshab Roy <utshab.roy@gmail.com>
2021-03-18 16:29:56 +0900

    basic listView implemented

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,25 @@ import 'package:flutter_ninja/app_screens/home.dart';
 void main() => runApp(MaterialApp(
       title: "Exploring UI Widget",
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: Scaffold(
+        body: getListView(),
+      ),
     ));
+
+Widget getListView() {
+  return ListView(
+    children: [
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text('Landscape'),
+        subtitle: Text('Beautiful view !'),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: () => print('tap on list tile'),
+      ),
+      ListTile(
+        leading: Icon(Icons.desktop_windows),
+        title: Text('Windows'),
+      ),
+    ],
+  );
+}
