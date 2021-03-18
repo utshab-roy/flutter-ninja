f70c87b277c9f5ead5798b06f7ee21be06b5f6b9
Utshab Roy <utshab.roy@gmail.com>
2021-03-18 11:43:24 +0900

    row, column, assets image, expanded widget implemented

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -3,17 +3,86 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.deepPurple,
-      child: Text(
-        "Flight",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          decoration: TextDecoration.none,
-          fontSize: 40.0,
-          fontFamily: 'Raleway',
-          fontWeight: FontWeight.w300,
+    return Center(
+      child: Container(
+        padding: EdgeInsets.only(
+          left: 10.0,
+          top: 40.0,
+        ),
+        alignment: Alignment.center,
+        color: Colors.deepPurple,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    "JAL",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 30.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w300,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    "From Japan to Bangladesh",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 20.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w300,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40.0),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    "Qatar",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 30.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w300,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    "From Bangladesh to Doha",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 20.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w300,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Image(
+              image: AssetImage(
+                'assets/sample-2.jpg',
+              ),
+              width: 100.0,
+              height: 100.0,
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ninja/app_screens/home.dart';
 
 void main() => runApp(MaterialApp(
-  title: "Exploring UI Widget",
-  home: Home(),
-));
+      title: "Exploring UI Widget",
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ));
